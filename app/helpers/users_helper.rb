a005bdf50044beb85ec33ac698f2223536d5d5a4
kazuhiro <q9fJ8neq@gmail.com>
2020-11-12 19:40:00 +0900

    improve condition in get_userimage method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def get_userimage(user)
     default_picture = "user/default_picture.png"
-    return user.picture.nil? || !File.exist?("public/#{user.picture.to_s}") ? 
-      default_picture : user.picture.to_s
+    return user.picture.nil? || !File.exist?(user.picture.path) ? 
+      default_picture : user.picture.url
   end
 end
