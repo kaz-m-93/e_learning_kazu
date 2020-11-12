676100e7d7e857480119bf0cd1f8329dd488e603
kazuhiro <q9fJ8neq@gmail.com>
2020-11-12 17:35:39 +0900

    [Function] Create Profile Edit Function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :check_login_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -25,8 +27,33 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+
+    unless params[:user][:picture].nil?
+      load_image = params[:user][:picture].read
+      file_name = "#{@user.id}.png"
+      File.binwrite("app/assets/images/user/#{file_name}", load_image)
+      params[:user][:picture] = file_name
+    end
+
+    if @user.update_attributes(user_params)
+      flash[:success] = "your account has been updated"
+      redirect_to user_path(@user)
+    else
+      render 'edit'
+    end
+  end
+
   private
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :picture)
+  end
+
+  def check_login_user
+    unless login_user?
+      flash[:danger] = "sonmething wrong with your operation"
+      redirect_to root_path
+    end
   end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,7 @@
 module UsersHelper
   def get_userimage(user)
     default_picture = "user/default_picture.png"
-    return user.picture.nil? ? default_picture : "user/#{user.picture}"
+    return user.picture.nil? || !File.exist?("app/assets/images/user/#{user.picture}") ? 
+      default_picture : "user/#{user.picture}"
   end
 end
