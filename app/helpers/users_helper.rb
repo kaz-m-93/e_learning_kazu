a5aee63670adeb5da45789c43c57f4423dbdad38
kaz-m-93 <73412921+kaz-m-93@users.noreply.github.com>
2020-11-13 15:08:34 +0900

    Function save images (#12)
    * Function save images
    
    * improve condition in get_userimage method

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def get_userimage(user)
     default_picture = "user/default_picture.png"
-    # To-Do:need function about save and display user image
-    return default_picture
+    return user.picture.nil? || !File.exist?(user.picture.path) ? 
+      default_picture : user.picture.url
   end
 end
