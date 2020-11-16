11c1526e8b89eead9ab45bc08c1ef6d945318a0a
kaz-m-93 <73412921+kaz-m-93@users.noreply.github.com>
2020-11-16 16:41:38 +0900

    [Layout][Admin] Create Add Category Page (#14)
    * [Layout][Admin] Create Add Category Page
    
    * fix css
    
    * add category model and fix param in form_for
    
    * fix indentation

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,8 @@
 class Admin::CategoriesController < ApplicationController
   def index
   end
+
+  def new
+    @category = Category.new
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_09_082908) do
+ActiveRecord::Schema.define(version: 2020_11_16_062114) do
+
+  create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
