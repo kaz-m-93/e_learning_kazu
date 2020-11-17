6b8d27b5c847194f6386b28f0233484e9c80fb4e
kaz-m-93 <73412921+kaz-m-93@users.noreply.github.com>
2020-11-17 15:19:13 +0900

    [Layout][Admin] Create add Words Page (#16)

---
+++ app/controllers/admin/words_controller.rb
@@ -0,0 +1,4 @@
+class Admin::WordsController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
-  root 'pages#home'
-  get '/about', to: 'pages#about'
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create' # solution to url's change, if error occurs
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create' # solution to url's change, if error occurs
-  delete '/logout', to: 'sessions#destroy'
-  get '/dashboard', to: 'users#dashboard'
-  
+  root "pages#home"
+  get "/about", to: "pages#about"
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create" # solution to url's change, if error occurs
+  get "/login", to: "sessions#new"
+  post "/login", to: "sessions#create" # solution to url's change, if error occurs
+  delete "/logout", to: "sessions#destroy"
+  get "/dashboard", to: "users#dashboard"
+
   resources :users
   namespace :admin do
-    resources :users, :categories
+    resources :users
+    resources :categories do
+      resources :words
+    end
   end
   resources :sessions
 end
