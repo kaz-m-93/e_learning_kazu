0177867c91dd2655cd9f30ca4e4db597874b52aa
kazuhiro <q9fJ8neq@gmail.com>
2020-11-18 17:44:32 +0900

    [Function][Admin] Create Management Words Function

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -13,20 +13,11 @@ class Admin::WordsController < ApplicationController
 
   def create
     @category = Category.find(params[:category_id])
-    @word = @category.words.build(word_params)
-
-    index = 0
-    unless params[:word][:correct_no].nil?
-      while index < @word.choices.size
-        @word.choices[index].is_correct = params[:word][:correct_no].to_i == index
-        index += 1
-      end
-    end
+    @word = set_correct_choice(@category.words.build(word_params), params[:word][:correct_no])
 
     if @word.save
       flash[:success] = "the word has been added into #{@category.title}"
-      # ↓ after create index of words, replace the path
-      redirect_to admin_categories_url
+      redirect_to admin_category_words_url(@category)
     else
       render "new"
     end
@@ -36,9 +27,39 @@ class Admin::WordsController < ApplicationController
     @word = Word.find(params[:id])
   end
 
+  def update
+    @word = set_correct_choice(Word.find(params[:id]), params[:word][:correct_no])
+
+    if @word.update_attributes(word_params)
+      flash[:success] = "the word has been edited"
+      redirect_to admin_category_words_url(@word.category_id)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @word = Word.find(params[:id])
+    @word.destroy
+    flash[:success] = "the word has been deleted from #{Category.find(params[:category_id]).title}"
+    redirect_to admin_category_words_url(@word.category_id)
+  end
+
   private
 
   def word_params
     params.require(:word).permit(:content, choices_attributes: [:id, :content, :is_correct])
+  end
+
+  def set_correct_choice(word, correct_no)
+    index = 0
+    unless correct_no.nil?
+      while index < word.choices.size
+        word.choices[index].is_correct = correct_no.to_i == index
+        index += 1
+      end
+    end
+
+    return word
   end
 end
