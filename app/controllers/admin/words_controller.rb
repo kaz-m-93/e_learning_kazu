37c717922a3ffb7f93f0ca453487de83dab8451f
kaz-m-93 <73412921+kaz-m-93@users.noreply.github.com>
2020-11-18 16:21:50 +0900

    [Layout][Admin] Create Management Words Page (#20)

--- app/controllers/admin/words_controller.rb
+++ app/controllers/admin/words_controller.rb
@@ -1,4 +1,8 @@
 class Admin::WordsController < ApplicationController
+  def index
+    @category = Category.find(params[:category_id])
+  end
+
   def new
     @category = Category.find(params[:category_id])
     @word = @category.words.build
