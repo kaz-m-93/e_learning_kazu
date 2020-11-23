12b82a54cc6d67bdc27d614286c428e44605274b
kazuhiro <q9fJ8neq@gmail.com>
2020-11-23 18:37:09 +0900

    add updating result function

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,11 +3,12 @@ class AnswersController < ApplicationController
 
   def new
     @lesson = Lesson.find(params[:lesson_id])
+    @lesson.update_attribute(:result, @lesson.answers.size) if @lesson.answers.size > 0
 
     if @lesson.next_word
       @answer = @lesson.answers.build
     else
-      if @lesson.words.size == 0
+      if @lesson.category.words.size == 0
         flash[:info] = "sorry, there is no words"
         redirect_to categories_url
       else
