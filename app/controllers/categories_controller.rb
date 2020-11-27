ccdf3abbfab3f7d65d72189627a17087b92a3397
kazuhiro <q9fJ8neq@gmail.com>
2020-11-27 16:43:29 +0900

    fix the sql more concisely

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,12 +2,16 @@ class CategoriesController < ApplicationController
   before_action :check_logged_in
 
   def index
+    if params[:filter_val].nil?
+      return @categories = Category.all
+    end
+
+    ids = login_user.learned_lessons.pluck(:category_id)
     if params[:filter_val].to_i == 1
-      @categories = Category.where(id: login_user.learned_lessons.pluck(:category_id))
+      @categories = Category.where(id: ids)
     elsif params[:filter_val].to_i == 2
-      @categories = Category.all - Category.where(id: login_user.learned_lessons.pluck(:category_id))
+      @categories = Category.where.not(id: ids)
     else
-      @categories = Category.all
     end
   end
 end
